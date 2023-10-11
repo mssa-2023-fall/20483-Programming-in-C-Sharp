4e82824543905ea14aa084fa0f96fd035ab7a3b3
karolyleo <leonard.a.karoly@gmail.com>
2023-10-10 22:22:30 -0500

    Completed Module 4 Exercise 1

--- Allfiles/Mod04/Labfiles/Starter/Exercise 1/GradesPrototype/Data/Grade.cs
+++ Allfiles/Mod04/Labfiles/Starter/Exercise 1/GradesPrototype/Data/Grade.cs
@@ -13,34 +13,121 @@ namespace GradesPrototype.Data
     // WPF Databinding requires properties
 
     // TODO: Exercise 1: Task 1a: Convert Grade into a class and define constructors
-    public struct Grade
+    public class Grade
     {
         public int StudentID { get; set; }
         public string AssessmentDate { get; set; }
         public string SubjectName { get; set; }
         public string Assessment { get; set; }
         public string Comments { get; set; }
+        public Grade(int studentID, string assessmentDate, string subjectName, string assessment, string comments)
+        {
+            StudentID = studentID;
+            AssessmentDate = assessmentDate;
+            SubjectName = subjectName;
+            Assessment = assessment;
+            Comments = comments;
+        }
+        public Grade()
+        {
+            StudentID = 0;
+            AssessmentDate = DateTime.Now.ToString("d");
+            SubjectName = "Math";
+            Assessment = "A";
+            Comments = ""; //Or String.Empty;
+        }
     }
 
     // TODO: Exercise 1: Task 2a: Convert Student into a class, make the password property write-only, add the VerifyPassword method, and define constructors
-    public struct Student
+    public class Student
     {
         public int StudentID { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+
+        private string _password = Guid.NewGuid().ToString();
+        public string Password { 
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public bool VerifyPassword(string pass)
+        {
+            return (String.Compare(pass, _password) == 0);
+        }
+
         public int TeacherID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        //Constructor
+        public Student(int studentID, string userName, string password, int teacherID, string firstName, string lastName)
+        {
+            StudentID = studentID;
+            UserName = userName;
+            Password = password;
+            TeacherID = teacherID;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //Default constructor
+        public Student()
+        {
+            StudentID = 0;
+            UserName = String.Empty;
+            Password = String.Empty;
+            TeacherID = 0;
+            FirstName = String.Empty;
+            LastName = String.Empty;
+        }
     }
 
     // TODO: Exercise 1: Task 2b: Convert Teacher into a class, make the password property write-only, add the VerifyPassword method, and define constructors
-    public struct Teacher
+    public class Teacher
     {
         public int TeacherID { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+
+        private string _password = Guid.NewGuid().ToString();
+        public string Password
+        {
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public bool VerifyPassword(string pass)
+        {
+            return (String.Compare(pass, _password) == 0);
+        }
+        
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Class { get; set; }
+
+        //Constructor
+        public Teacher(int teacherID, string userName, string password, string firstName, string lastName, string className)
+        {
+            TeacherID = teacherID;
+            UserName = userName;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Class = className;
+        }
+
+        //Default
+        public Teacher()
+        {
+            TeacherID = 0;
+            UserName = String.Empty;
+            Password = String.Empty;
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            Class = String.Empty;
+        }
     }
 }
